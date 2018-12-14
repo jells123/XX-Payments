94daabe6d5dbff1a3eea9326b6da1ba682383b89
Urszula Walinska <urszula.walinska@capgemini.com>
2018-12-14 16:42:52 +0100

    provided basic authentication

--- backend/xx_payments/settings.py
+++ backend/xx_payments/settings.py
@@ -102,6 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', )
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
--- backend/xx_payments/urls.py
+++ backend/xx_payments/urls.py
@@ -21,8 +21,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('kitty.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     #path('', include('users.urls'))
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
