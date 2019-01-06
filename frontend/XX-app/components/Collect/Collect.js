11d65ca6f61aaaee2acb8437c1c5165ebe3a15ef
jells123 <julka.bedziechowska@gmail.com>
2019-01-06 21:17:44 +0100

    Add Radio Group to Collect Screen

--- frontend/XX-app/components/Collect/Collect.js
+++ frontend/XX-app/components/Collect/Collect.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { AppRegistry, FlatList, StyleSheet, Text, View, TextInput } from 'react-native';
+import { AppRegistry, FlatList, TouchableOpacity, StyleSheet, Text, View, ScrollView, TextInput } from 'react-native';
 import { withNavigation } from 'react-navigation';
-import { MonoText } from '../../components/StyledText';
 import GlobalStyles from '../../constants/Style';
+
+import { RadioGroup } from 'react-native-btr';
 
 const BLUE = "#428AF8";
 const LIGHT_GRAY = "#D3D3D3";
@@ -51,24 +52,76 @@ class KittyAmountInput extends Component {
 }
 
 class Collect extends Component {
+  constructor(props) {
+    super(props);
+    this.state =  {
+      divideOptions:[
+        {
+          label: "Divide evenly",
+          color: '#fff',
+          size: 5
+        },
+        { 
+          label: "Custom...",
+          color: '#fff', 
+          size: 5
+        },
+      ],
+    };
+  }
+
+  onDivideOptionPress = data => this.setState({ data });
 
   render() {
     const { navigation } = this.props;
-    const activeUsers = navigation.getParam("activeUsers");
+    // const activeUsers = navigation.getParam("activeUsers");
+    const activeUsers = [
+      {
+        username: "user1",
+        id: 1
+      },
+      {
+        username: "user2",
+        id: 2
+      },
+      {
+        username: "user3",
+        id: 3
+      }
+    ];
 
     return (
-      <View style={GlobalStyles.container}>
+      <ScrollView style={GlobalStyles.container}>
         <View style={styles.kittyAmountContainer}>
           <Text style={styles.amountText}>Amount:</Text>
           <KittyAmountInput/>
           <Text style={styles.plnText}>PLN</Text>
         </View>
-        {/* <FlatList
+        <View style={styles.divideOptionsContainer}>
+          <RadioGroup
+            radioButtons={this.state.divideOptions}
+            onPress={this.onDivideOptionPress}
+            labelStyle={styles.divideOptionLabel}
+            style={{flex: 1}}
+            // https://github.com/ThakurBallary/react-native-btr/blob/master/docs/RADIO_GROUP.md
+          />
+        </View>
+        <View>
+        <TouchableOpacity style={styles.createButton}
+                  onPress={null}>
+          <Text style={styles.createText}>CREATE</Text>
+        </TouchableOpacity>
+        </View>
+
+      <View>
+        <Text style={GlobalStyles.commonText}>Choose participants:</Text>
+      </View>
+        <FlatList
           data={activeUsers}
           renderItem={({item}) => <Text style={styles.item}>{item.username}</Text>}
           keyExtractor={(item) => item.id.toString()}
-        /> */}
-      </View>
+        />
+      </ScrollView>
     );
   }
 }
@@ -86,21 +139,48 @@ const styles = StyleSheet.create({
     height: 100,
     alignItems: 'center',
     justifyContent: 'center', 
+    backgroundColor: '#8425a3',
+    marginTop: 15,
+    marginBottom: 15
   },
   kittyInput: {
     height: 40,
     paddingLeft: 6,
     flex: 1,
-    margin:20,
+    margin: 20,
     color: '#fff',
+    fontSize: 18
   },
   amountText: {
     ...GlobalStyles.commonText,
-    marginLeft: 30
+    marginLeft: 30,
+    fontSize: 18
   },
   plnText: {
     ...GlobalStyles.commonText,
-    marginRight: 30
+    marginRight: 30,
+    fontSize: 18
+  },
+  divideOptionsContainer: {
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  divideOptionLabel: {
+    color: '#fff'
+  },
+  createButton: {
+    ...GlobalStyles.buttonContainer,
+    marginTop: 20,
+    marginBottom: 10,
+    marginLeft: 60,
+    marginRight: 60,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  createText: {
+    ...GlobalStyles.buttonText,
+    fontSize: 17
   }
 })
 
--- frontend/XX-app/navigation/AppNavigator.js
+++ frontend/XX-app/navigation/AppNavigator.js
@@ -23,7 +23,7 @@ const AppNavigator = createStackNavigator(
     }
   },
   {
-    initialRouteName: "Login"
+    initialRouteName: "Collect"
   }
 );
 
