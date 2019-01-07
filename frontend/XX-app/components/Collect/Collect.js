8786ca1d423729548bcc9879a10663919c415cd8
jells123 <julka.bedziechowska@gmail.com>
2019-01-07 13:11:47 +0100

    Continue work on Collect screen (components communication)

--- frontend/XX-app/components/Collect/Collect.js
+++ frontend/XX-app/components/Collect/Collect.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { AppRegistry, FlatList, TouchableOpacity, StyleSheet, Text, View, ScrollView, TextInput } from 'react-native';
+import { FlatList, TouchableOpacity, StyleSheet, Text, View, ScrollView, TextInput } from 'react-native';
 import { withNavigation } from 'react-navigation';
-import GlobalStyles from '../../constants/Style';
-
 import { RadioGroup } from 'react-native-btr';
+
+import { CheckBox } from 'react-native-elements';
+import GlobalStyles from '../../constants/Style';
 
 const BLUE = "#428AF8";
 const LIGHT_GRAY = "#D3D3D3";
@@ -11,26 +12,29 @@ const LIGHT_GRAY = "#D3D3D3";
 class KittyAmountInput extends Component {
   // https://gist.github.com/mmazzarolo/77407406eea9a574c060662ab1bcac1f
   state = {
-    isFocused: false
+    isFocused: false,
+    amount: "0"
   };
 
-  handleFocus = event => {
+  _handleFocus = event => {
     this.setState({ isFocused: true });
     if (this.props.onFocus) {
       this.props.onFocus(event);
     }
   };
 
-  handleBlur = event => {
+  _handleBlur = event => {
     this.setState({ isFocused: false });
     if (this.props.onBlur) {
       this.props.onBlur(event);
     }
   };
 
-  onTextChanged(text) {
+  _handleTextChanged = (text) => {
     this.setState({
       amount: text.replace(/[^0-9.]/g, '')
+    }, () => {
+      this.props.handleKittyAmount(this.state.amount);
     })
   }
 
@@ -38,36 +42,105 @@ class KittyAmountInput extends Component {
     const { isFocused } = this.state;
     return (
       <TextInput style={styles.kittyInput}
-        placeholder="0.00"
         keyboardType='numeric'
         underlineColorAndroid={
           isFocused ? BLUE : LIGHT_GRAY
         }
-        onFocus={this.handleFocus}
-        onBlur={this.handleBlur}
-        onChangeText = {(text) => this.onTextChanged(text)}
+        onFocus={this._handleFocus}
+        onBlur={this._handleBlur}
+        value={this.state.amount}
+        onChangeText = {(text) => this._handleTextChanged(text)}
       />
     )
   }
 }
 
+class UserEntry extends Component {
+  
+/*
+to implement: componentWillReceiveProps...
+*/
+
+  state = {
+    checked : false
+  }
+
+  _handleChecked = () => {
+    this.setState({
+      checked: !this.state.checked
+    }, () => {
+      this.props.handleCheck(this.props.title);
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.userEntryContainer}>
+        <CheckBox
+          title={this.props.title}
+          checked={this.state.checked}
+          onPress={() => this._handleChecked()}
+        />
+        <TextInput
+          underlineColorAndroid={ LIGHT_GRAY }
+          style={{
+            flex: 1
+          }}
+          value={this.props.userAmount}
+        />
+      </View>
+    );
+  }
+}
+
 class Collect extends Component {
+
   constructor(props) {
     super(props);
+
+    var activeUsers = []
+    for (var i = 0; i < 10; i++) {
+      activeUsers.push({
+        username: "user"+(i).toString(),
+        id: i,
+        userAmount: "-"
+      })
+    }
+
     this.state =  {
-      divideOptions:[
+      divideOptions: [
         {
           label: "Divide evenly",
           color: '#fff',
-          size: 5
+          size: 5,
+          checked: true
         },
         { 
           label: "Custom...",
           color: '#fff', 
-          size: 5
+          size: 5, 
+          checked: false
         },
       ],
+      kittyAmount: 0,
+      activeUsers: activeUsers
     };
+  }
+
+  _handleKittyAmount = (amount) => {
+    this.setState(
+      { 
+        kittyAmount: amount 
+      },
+      () => {
+        // console.log(this.state.kittyAmount);
+      }
+    );
+  }
+
+  _handleUserEntryCheck = (entryName) => {
+    console.log(entryName);
+    console.log(this.state.activeUsers[parseInt(entryName[entryName.length - 1])]);
   }
 
   onDivideOptionPress = data => this.setState({ data });
@@ -75,26 +148,18 @@ class Collect extends Component {
   render() {
     const { navigation } = this.props;
     // const activeUsers = navigation.getParam("activeUsers");
-    const activeUsers = [
-      {
-        username: "user1",
-        id: 1
-      },
-      {
-        username: "user2",
-        id: 2
-      },
-      {
-        username: "user3",
-        id: 3
-      }
-    ];
+
 
     return (
-      <ScrollView style={GlobalStyles.container}>
+      <ScrollView 
+        style={GlobalStyles.container}
+        contentContainerStyle={{flex: 1}}
+      >
         <View style={styles.kittyAmountContainer}>
           <Text style={styles.amountText}>Amount:</Text>
-          <KittyAmountInput/>
+          <KittyAmountInput
+            handleKittyAmount={this._handleKittyAmount}
+          />
           <Text style={styles.plnText}>PLN</Text>
         </View>
         <View style={styles.divideOptionsContainer}>
@@ -102,25 +167,36 @@ class Collect extends Component {
             radioButtons={this.state.divideOptions}
             onPress={this.onDivideOptionPress}
             labelStyle={styles.divideOptionLabel}
-            style={{flex: 1}}
             // https://github.com/ThakurBallary/react-native-btr/blob/master/docs/RADIO_GROUP.md
           />
         </View>
-        <View>
-        <TouchableOpacity style={styles.createButton}
-                  onPress={null}>
-          <Text style={styles.createText}>CREATE</Text>
-        </TouchableOpacity>
+        <View style={styles.createButtonContainer}>
+          <TouchableOpacity style={styles.createButton}
+                    onPress={null}>
+            <Text style={styles.createText}>CREATE</Text>
+          </TouchableOpacity>
+          <Text style={GlobalStyles.commonText}>Choose participants:</Text>
         </View>
 
-      <View>
-        <Text style={GlobalStyles.commonText}>Choose participants:</Text>
-      </View>
+      <ScrollView 
+        style={{flex: 1, marginTop: 20, marginBottom: 10, marginLeft: 5, marginRight: 5}}
+        contentContainerStyle={{flex: 1}}
+      >
         <FlatList
-          data={activeUsers}
-          renderItem={({item}) => <Text style={styles.item}>{item.username}</Text>}
-          keyExtractor={(item) => item.id.toString()}
+          data={this.state.activeUsers}
+          renderItem={
+            ({item}) => 
+            <UserEntry
+              title={item.username}
+              handleCheck={this._handleUserEntryCheck}
+              userAmount={item.userAmount}
+            />
+          }
+          keyExtractor={
+            (item) => item.id.toString()
+          }
         />
+      </ScrollView>
       </ScrollView>
     );
   }
@@ -134,7 +210,7 @@ const styles = StyleSheet.create({
       height: 44,
   },
   kittyAmountContainer: {
-    flex: 1,
+    flex: 0.3,
     flexDirection: 'row',
     height: 100,
     alignItems: 'center',
@@ -162,7 +238,7 @@ const styles = StyleSheet.create({
     fontSize: 18
   },
   divideOptionsContainer: {
-    flex:1,
+    flex:0.3,
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -178,9 +254,16 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     paddingBottom: 10
   },
+  createButtonContainer: {
+    flex:0.3,
+  },
   createText: {
     ...GlobalStyles.buttonText,
     fontSize: 17
+  },
+  userEntryContainer: {
+    flex:1, 
+    flexDirection: 'row'
   }
 })
 
